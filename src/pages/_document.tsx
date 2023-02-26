2d512cd72975910832e56a162fd063fab3e10dbf
Abderrahmen Mhemed <mhemedabderrahmen@gmail.com>
2023-02-26 11:00:16 +0000

    ci: implement husky

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,19 @@
-import { createGetInitialProps } from '@mantine/next';
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-const getInitialProps = createGetInitialProps();
-
-export default class _Document extends Document {
-  static getInitialProps = getInitialProps;
-
+class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          {/* Any custom head tags */}
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
+
+export default MyDocument
