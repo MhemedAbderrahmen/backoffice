4fe7fe608130c6c815c02b9550a060d1ccf63226
Abderrahmen Mhemed <mhemedabderrahmen@gmail.com>
2023-02-26 10:56:48 +0000

    ci: implement husky

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,5 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import styles from '@/styles/Home.module.css';
-import Layout from '@/components/layout/layout';
 
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -16,9 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Layout>
-          <p>Hello World</p>
-        </Layout>
+
       </main>
     </>
   );
